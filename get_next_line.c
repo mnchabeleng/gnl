c8c3a4fdf598d063746913cbce403fbf5a6443aa
mnchabel <mnchabel@student.wethinkcode.co.za>
2019-06-29 17:37:27 +0200

    modified : get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,37 @@
 /*   By: mnchabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/21 09:42:57 by mnchabel          #+#    #+#             */
-/*   Updated: 2019/06/29 12:18:45 by mnchabel         ###   ########.fr       */
+/*   Updated: 2019/06/29 17:36:54 by mnchabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
 	static char	*file[2147483647];
 	char		buff[BUFF_SIZE + 1];
 	char		*temp;
-	int			ret;
+	size_t		ret;
 	int			endl;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || (!file[fd] && !(file[fd] = ft_strnew(1))) || !line)
 		return (-1);
-	while (ret = read(fd, buff, BUFF_SIZE))
+	while (!ft_strchr(file[fd], '\n') && (b = read(fd, buffer, BUFF_SIZE)))
+	{
+		buff[ret] = '\0';
+		temp = file[fd];
+		file[fd] = ft_strjoin(file[fd], buff);
+		ft_strdel(&temp);
+	}
+	if (ret == -1 || !*(temp = file[fd]))
+		return (ret == -1 ? -1 : 0);
+	if ((endl = (ft_strchr(file[fd], '\n') > 0)))
+		*line = ft_strsub(c[fd], 0, ft_strchr(file[fd], '\n') - file[fd]);
+	else
+		*line = ft_strdup(file[fd]);
+	file[fd] = ft_strsub(file[fd], (unsigned int)(ft_strlen(*line) + endl),
+			(size_t)(ft_strlen(file[fd]) - (ft_strlen(*line) + endl)));
+	ft_strdel(&temp);
 	return (0);
 }
