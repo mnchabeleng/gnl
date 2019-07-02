8c1c5bd1fc914d6be8054378006da2e3b59f441f
mnchabel <mnchabel@student.wethinkcode.co.za>
2019-07-02 10:39:31 +0200

    modified : get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mnchabel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/21 09:42:57 by mnchabel          #+#    #+#             */
-/*   Updated: 2019/06/30 14:45:57 by mnchabel         ###   ########.fr       */
+/*   Updated: 2019/07/02 10:38:55 by mnchabel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 int	get_next_line(const int fd, char **line)
 {
-	static char	*file[2147483647];
+	static char	*c[2147483647];
 	char		buff[BUFF_SIZE + 1];
 	char		*temp;
-	size_t		ret;
+	ssize_t		ret;
 	int			endl;
 
-	if (fd < 0 || (!file[fd] && !(file[fd] = ft_strnew(1))) || !line)
+	if (fd < 0 || (!c[fd] && !(c[fd] = ft_strnew(1))) || !line)
 		return (-1);
-	while (!ft_strchr(file[fd], '\n') && (ret = read(fd, buff, BUFF_SIZE)))
+	while (!ft_strchr(c[fd], '\n') && (ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[ret] = '\0';
-		temp = file[fd];
-		file[fd] = ft_strjoin(file[fd], buff);
+		temp = c[fd];
+		c[fd] = ft_strjoin(c[fd], buff);
 		ft_strdel(&temp);
 	}
-	if (ret == -1 || !*(temp = file[fd]))
+	if (ret == -1 || !*(temp = c[fd]))
 		return (ret == -1 ? -1 : 0);
-	if ((endl = (ft_strchr(file[fd], '\n') > 0)))
-		*line = ft_strsub(file[fd], 0, ft_strchr(file[fd], '\n') - file[fd]);
+	if ((endl = (ft_strchr(c[fd], '\n') > 0)))
+		*line = ft_strsub(c[fd], 0, ft_strchr(c[fd], '\n') - c[fd]);
 	else
-		*line = ft_strdup(file[fd]);
-	file[fd] = ft_strsub(file[fd], (unsigned int)(ft_strlen(*line) + endl),
-			(size_t)(ft_strlen(file[fd]) - (ft_strlen(*line) + endl)));
+		*line = ft_strdup(c[fd]);
+	c[fd] = ft_strsub(c[fd], (unsigned int)(ft_strlen(*line) + endl),
+			(size_t)(ft_strlen(c[fd]) - (ft_strlen(*line) + endl)));
 	ft_strdel(&temp);
-	return (0);
+	return (!(!c[fd] && !ft_strlen(*line)));
 }
